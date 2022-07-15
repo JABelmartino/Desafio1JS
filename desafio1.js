011f1286280b443afa800119197dd5a5a60758ca
Julian Belmartino <belmartinojulian@gmail.com>
2022-07-14 21:39:03 -0300

    Update desafio1.js

--- desafio1.js
+++ desafio1.js
@@ -1,39 +1,39 @@
-let Usuarios = class{
-    constructor(nombre, apellido, libros, mascotas){
-    this.nombre = nombre
-    this.apellido = apellido
-    this.libros = libros
-    this.mascotas = mascotas
-    }
-    getFullName(){
-        console.log(this.nombre + ' ' + this.apellido)
+const Usuarios = class{
+  constructor(nombre, apellido, libros, mascotas){
+  this.nombre = nombre
+  this.apellido = apellido
+  this.libros = libros
+  this.mascotas = mascotas
+  }
+getFullName(){
+  const nombreCompleto = `${this.nombre} ${this.apellido}`
+  return(nombreCompleto)
 }
-  addMascota(){
-    this.mascotas.unshift('loro')
-  }
-	countMascotas(){
-    console.log(usuario.mascotas.length)
-  }
-	addBook(){
-    this.libros.unshift({titulo:'Robinson Crusoe', autor: 'Johnson'})
-  }
-	getBookNames(){
-     let lista = this.libros.map(dato => dato.titulo)
-     return(lista)
-  }
+addMascota(nuevo){
+  this.mascotas.push(nuevo)
 }
-let usuario = new Usuarios('Pepe','Pipo',([{titulo:'El Viejo y El Mar', autor: 'Hemingway'},{titulo:'Moby Dick', autor:'Levington'}]),['dogo','chiuaua'])
+countMascotas(){
+  return(this.mascotas.length)
+}
+addBook(nuevoLibro){
+  this.libros.push(nuevoLibro)
+}
+getBookNames(){
+   const lista = this.libros.map(dato => dato.titulo)
+   return(lista)
+}
+}
+let usuario = new Usuarios('Pepe','Pipo',([{titulo:'El Viejo y El Mar', autor: 'Hemingway'},{titulo:'Moby Dick', autor:'Melville'}]),['dogo','chiuaua'])
 
 //Mostrar nombre completo
 usuario.getFullName()
 //Agregar un libro
-usuario.addBook()
+usuario.addBook({titulo:'Robinson Crusoe', autor: 'Defoe'})
 console.log(usuario.libros)
 //Agregar una mascota
-usuario.addMascota()
+usuario.addMascota('loro')
 console.log(usuario.mascotas)
 //Contador de mascotas
 usuario.countMascotas()
 //Lista de Libros
 usuario.getBookNames()
-console.log(usuario.lista)
